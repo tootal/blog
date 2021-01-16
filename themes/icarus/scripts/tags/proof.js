eecb68c9717ef28bf9b2590782ba6696995542be
tootal <tootal@yeah.net>
2021-01-16 16:03:07 +0800

    add proof style

--- themes/icarus/scripts/tags/proof.js
+++ themes/icarus/scripts/tags/proof.js
@@ -2,8 +2,8 @@
 
 module.exports = ctx => function(args, content) {
   content = ctx.render.renderSync({ text: content, engine: 'markdown' });
-  return `<div class="proof-container">PROOF. 
+  return `<div class="proof-container">
             <div class="proof-content">${content}</div>
-            <div class="proof-qed"></div>
+            <div class="proof-qed"><div class="proof-qed-icon"></div></div>
           </div>`;
 };
