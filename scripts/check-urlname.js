db51dbfa0517938225b1f7d095d3799990f4b43f
tootal <tootal@yeah.net>
2021-02-15 11:01:48 +0800

    skip render vx.json

--- scripts/check-urlname.js
+++ scripts/check-urlname.js
@@ -4,12 +4,13 @@ const logger = require('hexo-log')();
 
 hexo.extend.filter.register('before_post_render', function (data) {
     // 检查urlname
+    if (!data.source.endsWith('.md')) return data;
     if (data.layout === 'page') return data;
     if (data.urlname === undefined || (data.urlname === null)) {
-        if (data.title === undefined) {
-            console.log(data);
-        } else {
+        if (data.title) {
             logger.error('文章: 《' + data.title + '》没有设置urlname');
+        } else {
+            console.log(data.source);
         }
         process.exit(-1);
     }
--- source/_drafts/一月review.md
+++ source/_drafts/一月review.md
@@ -1,1 +1,15 @@
-# 一月review
+---
+title: 一月review
+urlname: 一月review
+toc: false
+categories:
+tags:
+date: 2021-01-31 15:45:29
+updated: 2021-01-31 15:45:29
+---
+
+![](../asset/二分图匹配，了解一下.cover.png)
+
+
+<!-- more -->
+
